8c172ec238c8f356455ab5d5ec26cae97ebdb090
Jacob Gillespie <jacobwgillespie@gmail.com>
2016-06-12 11:49:35 -0500

    Add README, renamed repo, and CDN

---
+++ README.md
@@ -0,0 +1,5 @@
+# bpsk31 [![Build Status](https://travis-ci.org/jacobwgillespie/bpsk31.com.svg?branch=master)](https://travis-ci.org/jacobwgillespie/bpsk31.com)
+
+Website about PSK31, the Amateur Radio digital operating mode.  Built automatically with Travis CI and Middleman.
+
+**See the live website:** [https://bpsk31.com](https://bpsk31.com)
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,12 +7,22 @@ fi
 
 cd build
 
-git init
-
+# Configure git
 git config user.name "Jacob Gillespie"
 git config user.email "jacobwgillespie@gmail.com"
 
+# Deploy to main site
+git init
 git add .
 git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/jacobwgillespie/bpsk31" master:gh-pages > /dev/null 2>&1
 
-git push --force --quiet "https://${GH_TOKEN}@github.com/jacobwgillespie/bpsk31.com" master:gh-pages > /dev/null 2>&1
+# Reset git
+rm -rf .git
+
+# Deploy to CDN
+git init
+echo "cdn.bpsk31.com" > CNAME
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/jacobwgillespie/bpsk31-cdn" master:gh-pages > /dev/null 2>&1
